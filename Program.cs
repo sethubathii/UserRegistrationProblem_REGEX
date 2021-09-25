fa0a1c58e79eead9018f1a23475eb704cb91d228
sethu <sethu@DESKTOP-24CTG5U>
2021-09-25 08:56:33 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem_Regex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserRegistrationProblem user = new UserRegistrationProblem();
+            user.Validation();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UserRegistrationProblem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem_Regex
+
+{/// <summary>
+/// UC9 creating regular expression for testing all email sample
+/// </summary>
+    class UserRegistrationProblem
+    {
+        //writing the valid pattern
+        string email = "^[a-zA-Z0-9]+[._+-]{0,1}[a-zA-Z0-9]+@[a-zA-Z0-9]{1,10}[.][a-zA-Z]{2,10}[.]*[a-zA-Z]*$";
+        string[] inputs = {"abc@yahoo.com","abc-100@yahoo.com", "abc.100@yahoo.com","abc111@abc.com", "abc-100@abc.net", "abc.100@abc.com.au","abc@1.com","abc@gmail.com.com", "abc+100@gmail.com",
+                            "abc","abc@.com.my","abc123@gmail.a","abc123@.com","abc123@.com.com",".abc@abc.com","abc()*@gmail.com","abc@%*.com","abc..2002@gmail.com","abc.@gmail.com","abc@abc@gmail.com","abc@gmail.com.1a","abc@gmail.com.aa.au" };
+        /// <summary>
+        /// Validation this instance.
+        /// </summary>
+        public void Validation()
+        {
+            Regex regex1 = new Regex(email);
+            Console.WriteLine("Validating Email id: ");
+            ItarateLoop(inputs, regex1);
+        }
+        public void ItarateLoop(string[] arr, Regex regex1)
+        {
+            for (int i = 0; i < arr.Length; i++)//validation Loop
+            {
+                bool result = regex1.IsMatch(arr[i]);
+                if (result)
+                {
+                    Console.WriteLine(arr[i] + ": is-->" + "Valid");
+                }
+                else
+                {
+                    Console.WriteLine(arr[i] + ": is-->" + "Invalid");
+                }
+            }
+        }
+    }
+}
